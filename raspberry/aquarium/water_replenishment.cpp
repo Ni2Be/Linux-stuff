d0a48ae143e014cf44a0e98ba35288487380ba5f
Nice2Bee SF <nice2beep@gmail.com>
2019-01-20 16:11:53 +0100

    water sensor logic

--- raspberry/aquarium/water_replenishment.cpp
+++ raspberry/aquarium/water_replenishment.cpp
@@ -14,14 +14,14 @@ Water_Replenishment::Water_Replenishment(int milliliter)
 void Water_Replenishment::check_and_pump()
 {
     std::cout << "checking..\n";
-    if (!sensor_1.is_wet() && !sensor_2.is_wet())
+    if (sensor_1.is_wet() || sensor_2.is_wet())
     {
+        std::cout << "at least one sensor is wet, doing nothing\n";
+    }
+    else
+    {   
         std::cout << "sensors not wet, start pumping\n";
         //500ml/55s 1ml/0.11s
         pump.pump_for(std::chrono::milliseconds(110 * m_milliliter));
-    }
-    else
-    {
-        std::cout << "sensors wet, doing nothing\n";
     }
 }

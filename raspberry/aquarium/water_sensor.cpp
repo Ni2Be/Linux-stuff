4a1c50346a274da53ed5ca2bc9c3584770b9826f
Nice2Bee SF <nice2beep@gmail.com>
2019-01-20 13:29:17 +0100

    comments

--- raspberry/aquarium/water_sensor.cpp
+++ raspberry/aquarium/water_sensor.cpp
@@ -35,7 +35,7 @@ bool Water_Sensor::is_wet()
 {
 	bool wet = false;
 	gpioWrite(m_output_pin, 1); /* on */
-	gpioSetPullUpDown(input_pin, PI_PUD_DOWN);
+	gpioSetPullUpDown(m_input_pin, PI_PUD_DOWN);
 	if(gpioRead(m_input_pin) == 1)
 	{
 		std::cout << "Pin: " << m_input_pin << ", state: " <<  gpioRead(m_input_pin) << ", is wet!\n";

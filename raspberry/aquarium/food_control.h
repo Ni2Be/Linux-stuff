7033c1418c636ffc307ff2f690773b82f7d73d1e
Nice2Bee SF <nice2beep@gmail.com>
2019-01-29 12:35:14 +0100

    food safety timer

--- raspberry/aquarium/food_control.h
+++ raspberry/aquarium/food_control.h
@@ -9,12 +9,12 @@ public:
         ~Food_Control();
 
         //
-        void rotate();
+        static void rotate();
 	static void stop_rotation();
 private:
         //holds the GPIO numbers
-        int m_input_pin;
-	int m_output_pin;
+        static int m_input_pin;
+	static int m_output_pin;
 };
 
 static void stop_rotation_cb(int, int, uint32_t, void*);
